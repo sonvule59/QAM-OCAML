8fc2787c93b871c9d315637761cb36cdb8718cde
Son Vu <40129621+sonvule59@users.noreply.github.com>
2024-11-16 22:46:10 -0600

    Update testCase.ml

--- REWRITE/testCase.ml
+++ REWRITE/testCase.ml
@@ -29,6 +29,52 @@ let test_check_process_equivalence _ =
   let p2 = Replication (ActionProcess ("b", NullProcess)) in
   assert_equal false (check_process_equivalence p1 p2)
 
+let test_check_resource_equivalence = 
+    let r1 = SimpleResource (SimpleResource NullResource) in
+    let r2 = SimpleResource (SimpleResource NullResource) in
+    assert_equal true (check_resource_equivalence r1 r2);
+  
+    let r1 = SimpleResource (SimpleResource NullResource) in
+    let r2 = SimpleResource NullResource in
+    assert_equal false (check_resource_equivalence r1 r2);
+  
+    let r1 = CombinedResource (SimpleResource NullResource, Quantum "input") in
+    let r2 = CombinedResource (SimpleResource NullResource, Quantum "input") in
+    assert_equal true (check_resource_equivalence r1 r2);
+  
+    let r1 = CombinedResource (SimpleResource NullResource, Quantum "input") in
+    let r2 = CombinedResource (SimpleResource NullResource, Quantum "input") in
+    assert_equal false (check_resource_equivalence r1 r2);
+  
+    let r1 = MeetOperation (SimpleResource NullResource, SimpleResource NullResource) in
+    let r2 = MeetOperation (SimpleResource NullResource, SimpleResource NullResource) in
+    assert_equal true (check_resource_equivalence r1 r2);
+  
+    let r1 = MeetOperation (SimpleResource NullResource, SimpleResource NullResource) in
+    let r2 = MeetOperation (SimpleResource NullResource, SimpleResource NullResource) in
+    assert_equal false (check_resource_equivalence r1 r2)
+  
+  let test_check_molecule_equivalence =
+    let m1 = ProcessMolecule NullProcess in
+    let m2 = ProcessMolecule NullProcess in
+    assert_equal true (check_molecule_equivalence m1 m2);
+  
+    let m1 = ProcessMolecule NullProcess in
+    let m2 = ProcessMolecule NullProcess in
+    assert_equal false (check_molecule_equivalence m1 m2);
+  
+    let m1 = ResourceMolecule (SimpleResource NullResource) in
+    let m2 = ResourceMolecule (SimpleResource NullResource) in
+    assert_equal true (check_molecule_equivalence m1 m2);
+  
+    let m1 = ResourceMolecule (SimpleResource NullResource) in
+    let m2 = ResourceMolecule (SimpleResource NullResource) in
+    assert_equal false (check_molecule_equivalence m1 m2);
+  
+    let m1 = ResourceMolecule (SimpleResource NullResource) in
+    let m2 = ProcessMolecule NullProcess in
+    assert_equal false (check_molecule_equivalence m1 m2)
+
 let suite =
   "TestCheckProcessEquivalence" >::: [
     "test_check_process_equivalence" >:: test_check_process_equivalence
